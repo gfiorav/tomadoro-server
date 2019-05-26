dc97e3d6a2440de6ed78ebeae9c1cff60b0c3ce7
Guido Fioravantti <guido.fioravantti@gmail.com>
2019-05-26 15:16:19 +0100

    Add middleware

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,18 @@ server.get("/", (request: express.Request, response: express.Response) => {
   response.send("welcome to the tomadoro service!");
 });
 
+const loggerMiddleware = (
+  request: express.Request,
+  reponse: express.Response,
+  next: express.NextFunction,
+) => {
+  process.stdout.write(`${request.method}: ${request.path}\n`);
+
+  next();
+};
+
+server.use(loggerMiddleware);
+
 server.listen(3000, () => {
-  process.stdout.write("listening on port 3000...");
+  process.stdout.write("listening on port 3000...\n");
 });
