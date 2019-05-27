f936b59193b55dccfec0b101b7f9ba57cc9afbc7
Guido Fioravantti <guido.fioravantti@gmail.com>
2019-05-27 23:28:51 +0100

    Remove email from session
    left over from previous copy-paste

--- server/models/session.ts
+++ server/models/session.ts
@@ -2,12 +2,10 @@ import sequelize from "sequelize";
 import db from "../db";
 
 const attributes: sequelize.ModelAttributes = {
-  email: {
-    allowNull: false,
-    type: sequelize.STRING,
-    validate: {
-      isEmail: true,
-    },
+  uuid: {
+    defaultValue: sequelize.UUIDV4,
+    primaryKey: true,
+    type: sequelize.UUID,
   },
 };
 
