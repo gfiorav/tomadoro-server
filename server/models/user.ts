273cf3f37ab97823a58a3c5c93dc5b3b2b67e86b
Guido Fioravantti <guido.fioravantti@gmail.com>
2019-05-27 23:27:16 +0100

    Add uuid

--- server/models/user.ts
+++ server/models/user.ts
@@ -9,6 +9,11 @@ const attributes: sequelize.ModelAttributes = {
       isEmail: true,
     },
   },
+  uuid: {
+    defaultValue: sequelize.UUIDV4,
+    primaryKey: true,
+    type: sequelize.UUID,
+  },
 };
 
 export = db.define("user", attributes);
