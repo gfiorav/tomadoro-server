b33f78d1ca133e83d38d0ab77191e3395d466d6c
Guido Fioravantti <guido.fioravantti@gmail.com>
2019-05-27 21:54:07 +0100

    Add Users controller

---
+++ server/controllers/users_controller.ts
@@ -0,0 +1,19 @@
+import sequelize from "sequelize";
+
+export = {
+  create: (request: sequelize.Request, response: sequelize.Response) => {
+    // TODO: Implement Users create
+  },
+  delete: (request: sequelize.Request, response: sequelize.Response) => {
+    // TODO: Implement Users delete
+  },
+  index: (request: sequelize.Request, response: sequelize.Response) => {
+    // TODO: Implement Users index
+  },
+  show: (request: sequelize.Request, response: sequelize.Response) => {
+    // TODO: Implement Users show
+  },
+  update: (request: sequelize.Request, response: sequelize.Response) => {
+    // TODO: Implement Users update
+  },
+};
