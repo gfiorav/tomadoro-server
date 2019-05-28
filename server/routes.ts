2d4da6ad2d7bc6553ffa13bbb3c634abbf7c7512
Guido Fioravantti <guido.fioravantti@gmail.com>
2019-05-28 23:48:12 +0100

    Routes must begin with /

--- server/routes.ts
+++ server/routes.ts
@@ -12,31 +12,31 @@ router.use((
   next();
 });
 
-router.post("users", (request: express.Request, response: express.Response): void => {
+router.post("/users", (request: express.Request, response: express.Response): void => {
   // TODO: Implement create User
 });
 
-router.use("users/:user_uuid", (request: express.Request, response: express.Response): void => {
+router.use("/users/:user_uuid", (request: express.Request, response: express.Response): void => {
   // TODO: Implement User loader
 });
 
-router.get("users/:user_uuid", (request: express.Request, response: express.Response): void => {
+router.get("/users/:user_uuid", (request: express.Request, response: express.Response): void => {
   // TODO: Implement show User
 });
 
-router.put("users/:user_uuid", (request: express.Request, response: express.Response): void => {
+router.put("/users/:user_uuid", (request: express.Request, response: express.Response): void => {
   // TODO: Implement update User
 });
 
-router.delete("users/:user_uuid", (request: express.Request, response: express.Response): void => {
+router.delete("/users/:user_uuid", (request: express.Request, response: express.Response): void => {
   // TODO: Implement delete User
 });
 
-router.post("sessions", (request: express.Request, response: express.Response): void => {
+router.post("/sessions", (request: express.Request, response: express.Response): void => {
   // TODO: Implement create Session
 });
 
-router.delete("sessions/:session_uuid", (request: express.Request, response: express.Response): void => {
+router.delete("/sessions/:session_uuid", (request: express.Request, response: express.Response): void => {
   // TODO: Implement delete Session
 });
 
